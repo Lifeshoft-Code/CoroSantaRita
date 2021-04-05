5de09cb324ac6c7ba11e5101a05d22824980a4da
Lifeshoft-Code <77507489+Lifeshoft-Code@users.noreply.github.com>
2021-04-05 12:11:51 -0400

    Add files via upload

---
+++ js/loader.js
@@ -0,0 +1,8 @@
+//loader
+window.addEventListener('load', function(){
+    let loaderContainer = document.getElementById('loader');
+    loaderContainer.style.display = 'none';
+})
+
+
+
---
+++ js/mainMenuFuctions.js
@@ -0,0 +1,61 @@
+//menu
+let btnMenu = document.getElementById('btn-icon-menu');
+let menuOpen = false;
+btnMenu.addEventListener("click", function(){
+    if(!menuOpen){
+        btnMenu.classList.add('open');
+        menuOpen = true;
+    }else{
+        btnMenu.classList.remove('open')
+        menuOpen = false;
+    }
+    
+    let menuItems = document.getElementById('menu-Items');
+    menuItems.classList.toggle('appear');
+
+});
+//mutacion de menu
+
+window.addEventListener('load', function(){
+    var navbar = document.getElementById('main-menu');
+    document.addEventListener('scroll', function(){
+        
+        if(scrollY >= 200){
+            navbar.className = 'menu-modified';
+        }
+        else if(scrollY >= 0){
+            
+
+            navbar.className = 'main-menu';
+            
+        }
+
+        if(innerWidth <= 900 ){
+            
+            navbar.className = 'main-menu';
+        }
+    })
+})
+//submenu
+
+let btnSubMenu = document.getElementById('btn-submenu');
+btnSubMenu.addEventListener('click', (e) =>{
+    e.preventDefault();
+    
+    let subMenu = document.getElementById('submenu');
+    subMenu.classList.add('submenu-appear');
+   
+    btnSubMenu.style.color = 'var(--color-heading)';
+
+    
+    
+        
+    
+    
+    
+    
+});
+let submenuBtnCancel = document.getElementById('submenu-btn-cancel');
+    submenuBtnCancel.addEventListener('click', () =>{
+        submenuBtnCancel.classList.remove('submenu-appear');
+    });
