b37b60578d4c65b3954987d46e87a6beaf12cf7f
Lifeshoft-Code <77507489+Lifeshoft-Code@users.noreply.github.com>
2021-04-05 19:35:07 -0400

    Update mainMenuFuctions.js

--- js/mainMenuFuctions.js
+++ js/mainMenuFuctions.js
@@ -55,6 +55,12 @@ btnSubMenu.addEventListener('click', (e) =>{
     
     
 });
+
+let btnCloseSubMenu= document.getElementById('submenu-btn-cancel');
+btnCloseSubMenu.addEventListener('click', function(){
+    let subMenu = document.getElementById('submenu');
+    subMenu.classList.remove('submenu-appear');
+})
 let submenuBtnCancel = document.getElementById('submenu-btn-cancel');
     submenuBtnCancel.addEventListener('click', () =>{
         submenuBtnCancel.classList.remove('submenu-appear');
